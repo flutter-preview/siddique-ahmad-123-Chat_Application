8c0caa09758140e52adc2304632461ee2ec2437a
Siddique Ahmad <ahmadsiddique7073@gmail.com>
2023-06-27 14:54:16 +0530

    2nd_Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:chatapplication/models/Firebase_helper.dart';
+import 'package:chatapplication/models/user_model.dart';
 import 'package:chatapplication/pages/LoginPage.dart';
 import 'package:chatapplication/pages/completeProfilepage.dart';
 import 'package:chatapplication/pages/signupPage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'HomePage.dart';
@@ -8,9 +11,31 @@ import 'HomePage.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
  await  Firebase.initializeApp();
+
+  User? currentUser =FirebaseAuth.instance.currentUser;
+ if(currentUser!=null) {
+  //logged in
+  UserModel? thisUserModel = await FirebaseHelper.getUserModelById
+  (currentUser.uid);
+
+  if(thisUserModel!=null)
+  runApp(MyAppLoggedIn(userModel:thisUserModel , firebaseUser: currentUser));
+
+  else{
+  //not loggedin
   runApp(const MyApp());
+ }
+
+ }
+ else{
+  //not loggedin
+  runApp(const MyApp());
+ }
+
+  
 }
 
+//NOT LOGGED IN
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -20,6 +45,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
       home: LoginPage(),
+    );
+  }
+}
+
+//ALREADY LOGGED IN:
+
+class MyAppLoggedIn extends StatelessWidget {
+ final UserModel userModel;
+final User firebaseUser;
+
+  const MyAppLoggedIn({super.key, required this.userModel, required this.firebaseUser});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      home: HomePage(userModel: userModel, firebaseUser: firebaseUser),
     );
   }
 }
---
+++ lib/models/Firebase_helper.dart
@@ -0,0 +1,26 @@
+import 'package:chatapplication/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class FirebaseHelper {
+
+ static   Future<UserModel?> getUserModelById (String uid) async {
+ UserModel? userModel;
+
+ DocumentSnapshot docSnap = await FirebaseFirestore.instance.collection
+ ("users").doc(uid).get();
+
+if(docSnap.data() != null){
+  userModel = UserModel.fromMap(docSnap.data() as Map<String,dynamic>);
+
+}
+
+return userModel;
+
+}
+
+
+
+
+
+}
